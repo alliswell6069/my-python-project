2fb9077502dcce7f23e6331520e641ca9abb7694
alliswell6069 <alliswell6069@gmail.com>
2022-08-25 03:32:09 +0000

    divided

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-s program adds two numbers
+#  program adds two numbers
 
 num1 = 1.5
 num2 = 6.3
@@ -20,3 +20,11 @@ difference = num1 - num2
 
 # Display the difference
 print('The difference of {0} and {1} is {2}'.format(num1, num2, difference))
+
+# divided two numbers
+divided = num1 / num2
+
+
+# Display the divided
+print('The divided of {0} and {1} is {2}'.format(num1, num2, divided))
+
